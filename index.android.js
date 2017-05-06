cb4c79d4fc9632b5e8d5038d92608c229038a85d
vectorzero <w3298028@163.com>
2017-05-07 01:28:03 +0800

    test

--- index.android.js
+++ index.android.js
@@ -4,5 +4,19 @@ import App from './app';
 
 AppRegistry.registerComponent('xxxxx', () => App);
 
-// change xxxxx to your project name
-//AppRegistry.registerComponent('xxxxx', () => AntdMobieDemo);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
